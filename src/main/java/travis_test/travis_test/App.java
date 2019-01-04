74844cde1b91abd87192aa439ec4baef7c3d2667
maddog <maddog@mdui-MacBook-Pro.local>
2019-01-04 17:05:06 +0900

    new test

---
+++ src/main/java/travis_test/travis_test/App.java
@@ -0,0 +1,13 @@
+package travis_test.travis_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
